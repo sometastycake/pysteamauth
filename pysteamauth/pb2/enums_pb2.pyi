08cf601097ab5005a08dc39b4f356e496cb18578
Mike M <16300181+sometastycake@users.noreply.github.com>
2023-01-25 13:21:51 +0300

    Remove old proto

--- pysteamauth/pb2/enums_pb2.pyi
+++
@@ -1,7 +0,0 @@
-from enum import IntEnum
-
-
-class ESessionPersistence(IntEnum):
-    k_ESessionPersistence_Invalid: int = -1
-    k_ESessionPersistence_Ephemeral: int = 0
-    k_ESessionPersistence_Persistent: int = 1
