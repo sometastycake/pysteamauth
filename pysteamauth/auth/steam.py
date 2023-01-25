9ae1de299e6ad71de1a4597a69d019902972d5ef
Mike M <16300181+sometastycake@users.noreply.github.com>
2023-01-25 21:17:25 +0300

    Add BaseSteam

--- pysteamauth/auth/steam.py
+++ pysteamauth/auth/steam.py
@@ -1,4 +1,6 @@
 import base64
+
+import aiohttp
 import binascii
 import hashlib
 import hmac
@@ -8,12 +10,13 @@ from struct import pack
 from typing import (
     Any,
     Mapping,
-    Optional,
+    Optional, Dict,
 )
 
 import rsa
 from aiohttp import FormData
 from bitstring import BitArray
+from google.protobuf.message import Message
 from urllib3.util import parse_url
 
 from pysteamauth.abstract import (
@@ -35,10 +38,114 @@ from pysteamauth.pb.steammessages_auth.steamclient_pb2 import (
     CAuthentication_PollAuthSessionStatus_Response,
     CAuthentication_UpdateAuthSessionWithSteamGuardCode_Request,
     EAuthSessionGuardType,
-    EAuthTokenPlatformType,
+    EAuthTokenPlatformType, CAuthentication_AccessToken_GenerateForApp_Response,
+    CAuthentication_AccessToken_GenerateForApp_Request,
 )
 
 from .schemas import FinalizeLoginStatus
+
+
+def pbmessage_to_request(msg: Message) -> str:
+    return str(base64.b64encode(msg.SerializeToString()), 'utf8')
+
+
+class BaseSteam:
+
+    def __init__(
+            self,
+            request_strategy: Optional[RequestStrategyAbstract] = None,
+            cookie_storage: Optional[CookieStorageAbstract] = None,
+    ):
+        if request_strategy is not None:
+            self._requests = request_strategy
+        else:
+            self._requests = BaseRequestStrategy(raise_for_status=True)
+        self._storage = (
+            cookie_storage
+                if cookie_storage is not None else BaseCookieStorage()
+        )
+
+    async def _poll_auth_session_status(
+            self,
+            client_id: int,
+            request_id: bytes,
+    ) -> CAuthentication_PollAuthSessionStatus_Response:
+        message = CAuthentication_PollAuthSessionStatus_Request(
+            client_id=client_id,
+            request_id=request_id,
+        )
+        response = await self._requests.bytes(
+            method='POST',
+            url='https://api.steampowered.com/IAuthenticationService/PollAuthSessionStatus/v1',
+            data=FormData(
+                fields=[
+                    ('input_protobuf_encoded', pbmessage_to_request(message)),
+                ],
+            ),
+        )
+        return CAuthentication_PollAuthSessionStatus_Response.FromString(response)
+
+    async def _refresh_access_token(
+            self,
+            steamid: int,
+            access_token: str,
+            refresh_token: str,
+    ) -> CAuthentication_AccessToken_GenerateForApp_Response:
+        message = CAuthentication_AccessToken_GenerateForApp_Request(
+            steamid=steamid,
+            refresh_token=refresh_token,
+        )
+        response = await self._requests.bytes(
+            method='POST',
+            url='https://api.steampowered.com/IAuthenticationService/GenerateAccessTokenForApp/v1',
+            params={
+                'access_token': access_token,
+            },
+            data=FormData(
+                fields=[
+                    ('input_protobuf_encoded', pbmessage_to_request(message)),
+                ],
+            ),
+        )
+        return CAuthentication_AccessToken_GenerateForApp_Response.FromString(response)
+
+    async def _finalize_login(self, refresh_token: str, sessionid: str) -> FinalizeLoginStatus:
+        response = await self._requests.text(
+            method='POST',
+            url='https://login.steampowered.com/jwt/finalizelogin',
+            data=FormData(
+                fields=[
+                    ('nonce', refresh_token),
+                    ('sessionid', sessionid),
+                    ('redir', 'https://steamcommunity.com/login/home/?goto='),
+                ],
+            ),
+        )
+        # TODO '{"success":false,"step":"nonce","error":8}'
+        return FinalizeLoginStatus.parse_raw(response)
+
+    async def _set_token(self, url: str, nonce: str, auth: str, steamid: int) -> aiohttp.ClientResponse:
+        return await self._requests.request(
+            method='POST',
+            url=url,
+            data=FormData(
+                fields=[
+                    ('nonce', nonce),
+                    ('auth', auth),
+                    ('steamID', str(steamid)),
+                ],
+            ),
+        )
+
+    async def enumerate_tokens(self, access_key: str) -> Dict:
+        response = await self._requests.bytes(
+            method='POST',
+            url='https://api.steampowered.com/IAuthenticationService/EnumerateTokens/v1',
+            params={
+                'access_token': access_key,
+            }
+        )
+        return json.loads(response)
 
 
 class Steam:
