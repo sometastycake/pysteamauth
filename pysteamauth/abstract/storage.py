bb67f10e41d7dd435de901ca4b9e92d707e84ec9
Mike M <16300181+sometastycake@users.noreply.github.com>
2023-01-27 01:52:50 +0300

    Replace Mapping to Dict

--- pysteamauth/abstract/storage.py
+++ pysteamauth/abstract/storage.py
@@ -2,15 +2,15 @@ from abc import (
     ABC,
     abstractmethod,
 )
-from typing import Mapping
+from typing import Dict
 
 
 class CookieStorageAbstract(ABC):
 
     @abstractmethod
-    async def set(self, key: str, cookies: Mapping[str, Mapping[str, str]]) -> None:  # noqa:U100
+    async def set(self, key: str, cookies: Dict[str, Dict[str, str]]) -> None:  # noqa:U100
         ...
 
     @abstractmethod
-    async def get(self, key: str, domain: str) -> Mapping[str, str]:  # noqa:U100
+    async def get(self, key: str, domain: str) -> Dict[str, str]:  # noqa:U100
         ...
--- pysteamauth/base/storage.py
+++ pysteamauth/base/storage.py
@@ -1,7 +1,4 @@
-from typing import (
-    Dict,
-    Mapping,
-)
+from typing import Dict
 
 from pysteamauth.abstract import CookieStorageAbstract
 
@@ -9,12 +6,12 @@ from pysteamauth.abstract import CookieStorageAbstract
 class BaseCookieStorage(CookieStorageAbstract):
 
     def __init__(self):
-        self.cookies: Dict[str, Mapping[str, Mapping[str, str]]] = {}
+        self.cookies: Dict[str, Dict[str, Dict[str, str]]] = {}
 
-    async def set(self, key: str, cookies: Mapping[str, Mapping[str, str]]) -> None:
+    async def set(self, key: str, cookies: Dict[str, Dict[str, str]]) -> None:
         self.cookies[key] = cookies
 
-    async def get(self, key: str, domain: str) -> Mapping[str, str]:
+    async def get(self, key: str, domain: str) -> Dict[str, str]:
         cookies = self.cookies.get(key)
         if not cookies:
             return {}
