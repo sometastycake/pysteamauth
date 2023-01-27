4323ee3e3627f35b7fb9728d7c5fd5b7b9697d48
Mike M <16300181+sometastycake@users.noreply.github.com>
2023-01-27 03:28:37 +0300

    Add types of cookies

--- pysteamauth/abstract/storage.py
+++ pysteamauth/abstract/storage.py
@@ -5,12 +5,17 @@ from abc import (
 from typing import Dict
 
 
+DOMAIN_TYPE = str
+COOKIE_TYPE = Dict[str, str]
+COOKIES_TYPE = Dict[DOMAIN_TYPE, COOKIE_TYPE]
+
+
 class CookieStorageAbstract(ABC):
 
     @abstractmethod
-    async def set(self, key: str, cookies: Dict[str, Dict[str, str]]) -> None:  # noqa:U100
+    async def set(self, key: str, cookies: COOKIES_TYPE) -> None:  # noqa:U100
         ...
 
     @abstractmethod
-    async def get(self, key: str, domain: str) -> Dict[str, str]:  # noqa:U100
+    async def get(self, key: str, domain: str) -> COOKIE_TYPE:  # noqa:U100
         ...
--- pysteamauth/base/storage.py
+++ pysteamauth/base/storage.py
@@ -1,17 +1,21 @@
 from typing import Dict
 
 from pysteamauth.abstract import CookieStorageAbstract
+from pysteamauth.abstract.storage import (
+    COOKIE_TYPE,
+    COOKIES_TYPE,
+)
 
 
 class BaseCookieStorage(CookieStorageAbstract):
 
     def __init__(self):
-        self.cookies: Dict[str, Dict[str, Dict[str, str]]] = {}
+        self.cookies: Dict[str, COOKIES_TYPE] = {}
 
-    async def set(self, key: str, cookies: Dict[str, Dict[str, str]]) -> None:
+    async def set(self, key: str, cookies: COOKIES_TYPE) -> None:
         self.cookies[key] = cookies
 
-    async def get(self, key: str, domain: str) -> Dict[str, str]:
+    async def get(self, key: str, domain: str) -> COOKIE_TYPE:
         cookies = self.cookies.get(key)
         if not cookies:
             return {}
